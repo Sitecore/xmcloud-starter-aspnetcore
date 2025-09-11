aabc2e94fedd831056f9a2c8b638efb360c542f9
Nelson Joel Raj Jesudos <njesudos@horizontalintegration.com>
2025-09-11 13:25:15 +0400

    updated the content resolver

---
+++ authoring/platform/ContentResolvers/CardContentResolver.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.LayoutService.Configuration;
+using Sitecore.LayoutService.ItemRendering.ContentsResolvers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+
+namespace XmCloudAspNetCoreStarter.ContentResolvers
+{
+    public class CardResolver : RenderingContentsResolver
+    {
+        public override object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
+        {
+
+            return new
+            {
+                Title = rendering.Item?.Fields["Title"]?.Value ?? string.Empty,
+                Description = rendering.Item?.Fields["Description"]?.Value ?? string.Empty,
+                Image = rendering.Item?.Fields["Image"]?.Value ?? string.Empty,
+                Link = rendering.Item?.Fields["Link"]?.Value ?? string.Empty
+
+            };    
+        }
+    }
+}
